1ca8621da8cb6ac87e9b0114bff7ea1184db5f59
Juan Carlos <juancafe2@gmail.com>
2017-02-18 18:07:48 -0400

    Flip to win flip a bit to find the highest sequences of 1s

---
+++ concepts_and_algorithms/bit_manipulation/flip_to_win.js
@@ -0,0 +1,49 @@
+function longestSequence(n) {
+  if (n === -1) {
+    return 32;
+  }
+  let seq = getAlternativeSequences(n);
+
+   let  maxSeq = 1;
+   for (let i = 0; i < seq.length; i += 2) {
+      let seqZeros = seq[i];
+      let actualSeq = 0;
+      let rightOnes = i + 1 < seq.length ? seq[i + 1]:0;
+      let leftOnes = i - 1 >= 0  ? seq[i - 1]:0;
+      
+      if (seqZeros === 1) {
+        actualSeq = rightOnes + leftOnes + 1;
+      }
+      if (seqZeros > 1) {
+        actualSeq = 1 + Math.max(rightOnes, leftOnes);
+
+
+      } else if (seqZeros === 0) {
+        let actualSeq = Math.max(rightOnes, leftOnes);
+      }
+
+      maxSeq = Math.max(maxSeq, actualSeq);
+   }
+   console.log(seq)
+   return maxSeq;
+}
+
+function getAlternativeSequences(n) {
+  let seqs = [];
+  let searchingFor = 0;
+  counter = 0;
+
+  for (let i = 0; i < 32; i++) {
+    if ((n & 1) != searchingFor) {
+      seqs.push(counter);
+      searchingFor = n & 1; // Flip 0 to 1 or 1 to 0
+      counter = 0; 
+    }
+    counter++;
+    n >>>= 1;
+  }
+  seqs.push(counter);
+  return seqs;
+}
+
+console.log(longestSequence(540));
