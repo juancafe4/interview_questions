896768c38957485b82d3455b49d62e6c04eaf4c0
Juan Carlos <juancafe2@gmail.com>
2017-02-25 16:52:06 -0400

    Permutation with duplication

---
+++ concepts_and_algorithms/recursion_dynamic_programming/permutation_with_dups.js
@@ -0,0 +1,40 @@
+function perms(str) {
+  let map = buildTable(str);
+  let result = [];
+  permUitls(map, "", str.length, result);
+  console.log(result)
+}
+
+function buildTable(str) {
+  let map = {};
+
+  for (let s of str) {
+    if (!map[s]) {
+      map[s] = 0;
+    }
+    map[s]++;
+  }
+  return map;
+}
+
+// Create an object or table the put the repeated characters
+// go for every character recurservly creating a permutation of each
+// one.
+function permUitls(map, prefix, remainder, result) {
+  if (remainder === 0) {
+    result.push(prefix);
+    return;
+  }
+  for (let key in map) {
+    let c = key;
+    let count = map[key];
+
+    if (count > 0) {
+      map[key] = count - 1;
+      console.log('Prefix', prefix);
+      permUitls(map, prefix + c, remainder - 1, result);
+      map[key] = count;
+    }
+  }
+}
+perms('AABB')
