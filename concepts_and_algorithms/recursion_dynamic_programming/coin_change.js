c21f9f744ab864672d2b8b5d0c2e6cd4231c3dd0
Juan Carlos <juancafe2@gmail.com>
2017-02-26 21:43:59 -0400

    Improve coin change O(N)

--- concepts_and_algorithms/recursion_dynamic_programming/coin_change.js
+++ concepts_and_algorithms/recursion_dynamic_programming/coin_change.js
@@ -31,34 +31,65 @@
 // }
 // console.log(coin_change(10))
 
-// Using Dynamic Programming
-
+// Dynamic Programming memo
 function  coin_change(n) {
   let denons = [25, 10, 5, 1];
-  
-  var table = new Array(n + 1);
-  for (var i = 0; i < n + 1; i++) {
-      table[i] = new Array(denons.length);
-  }
-  // Fill the enteries for 0 value case (n = 0)
-  for (let i = 0; i< denons.length; i++) {
-    table[0][i] = 1;
-  }
-  // Fill rest of the table enteries in bottom up manner 
-  for (let i = 1; i < n + 1; i++) {
-    for (let j = 0; j < denons.length; j++) {
-      let x = i - denons[j] >= 0 ? table[i - denons[j]][j]:0;
-      let y = j >= 1 ? table[i][j - 1]:0 ;
-      table[i][j] = x  + y
-    }
-  }
+  let memo = {}
+  return coin_change_util(denons, denons.length, n, memo);
 
-  return table[n][denons.length - 1];
 }
 
 
+function coin_change_util(denons, len, n, memo) {
+  let str = len + '-' + n;
+  if (n < 0) {
+    memo[str] = 0;
+    return memo[str];
+  }
+  if (n === 0) {
+    memo[str] = 1;
+    return memo[str];
+  }
+  if (len <= 0 && n > 0) {
+    memo[str] = 0;
+    return memo[str];
+  }
+  if (!memo[str]) {
+   memo[str] = coin_change_util(denons, len - 1, n, memo) 
+        + coin_change_util(denons, len, n - denons[len - 1], memo);
+  }
+  return memo[str]
+}
+console.log(coin_change(5))
 
-console.log(coin_change(25))
+// Using Dynamic Programming
+
+//   function  coin_change(n) {
+//     let denons = [25, 10, 5, 1];
+    
+//     var table = new Array(n + 1);
+//     for (var i = 0; i < n + 1; i++) {
+//         table[i] = new Array(denons.length);
+//     }
+//     // Fill the enteries for 0 value case (n = 0)
+//     for (let i = 0; i< denons.length; i++) {
+//       table[0][i] = 1;
+//     }
+//     // Fill rest of the table enteries in bottom up manner 
+//     for (let i = 1; i < n + 1; i++) {
+//       for (let j = 0; j < denons.length; j++) {
+//         let x = i - denons[j] >= 0 ? table[i - denons[j]][j]:0;
+//         let y = j >= 1 ? table[i][j - 1]:0 ;
+//         table[i][j] = x  + y
+//       }
+//     }
+
+//     return table[n][denons.length - 1];
+//   }
+
+
+
+// console.log(coin_change(25))
 
 // function coin_change(n) {
 //   let denons = [25, 10, 5, 1];
