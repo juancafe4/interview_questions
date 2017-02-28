b78cb13e1b5c74ef2a33f5c90ecbd0b3c786cd95
Juan Carlos <juancafe2@gmail.com>
2017-02-27 21:36:57 -0400

    Backtracking

--- concepts_and_algorithms/recursion_dynamic_programming/backtracking.js
+++ concepts_and_algorithms/recursion_dynamic_programming/backtracking.js
@@ -1,63 +1,43 @@
 function isSafe(board, row, col) {
-  // // Check from left to right
-  // for (let i = 0; i < board.length; i++) {
-  //   if (board[row][i]) {
-  //     return false;
-  //   }
-  // }
-  // // Top to bottom
-  // for (let i = 0; i < board.length; i++) {
-  //   if (board[i][row]) {
-  //     return false;
-  //   }
-  // }
+  // Check from left to right
+  for (let i = 0; i < board.length; i++) {
+    if (board[row][i]) {
+      return false;
+    }
+  }
+  // Top to bottom
+  for (let i = 0; i < board.length; i++) {
+    if (board[i][col]) {
+      return false;
+    }
+  }
 
-  // // Upper left diagnoal
-  // for (let i = row, j = col; i >= 0 && j >= 0; i--, j--) {
-  //   if (board[i][j]) {
-  //     return false;
-  //   }
-  // }
+  // Upper left diagnoal
+  for (let i = row, j = col; i >= 0 && j >= 0; i--, j--) {
+    if (board[i][j]) {
+      return false;
+    }
+  }
 
-  // // Lower left diagonal 
-  // for (i=row, j=col; j >= 0 && i < board.length; i++, j--) {
-  //   if (board[i][j]) {
-  //     return false;
-  //   }
-  // }
-  // // Lower right diagnoal
-  // for (let i = row, j = col; i < board.length && j < board.length; i++, j++) {
-  //   if (board[i][j]) {
-  //     return false;
-  //   }
-  // }
-  // // Upper right  diagonal 
-  // for (i=row, j=col; j < board.length && i >= 0; i--, j++) {
-  //   if (board[i][j]) {
-  //     return false;
-  //   }
-  // }
-  // console.log('board', board)
-  // return true;
-
-  let i, j;
- 
-    /* Check this row on left side */
-    for (i = 0; i < col; i++)
-        if (board[row][i])
-            return false;
- 
-    /* Check upper diagonal on left side */
-    for (i=row, j=col; i>=0 && j>=0; i--, j--)
-        if (board[i][j])
-            return false;
- 
-    /* Check lower diagonal on left side */
-    for (i=row, j=col; j>=0 && i<board.length; i++, j--)
-        if (board[i][j])
-            return false;
- 
-    return true;
+  // Lower left diagonal 
+  for (i=row, j=col; j >= 0 && i < board.length; i++, j--) {
+    if (board[i][j]) {
+      return false;
+    }
+  }
+  // Lower right diagnoal
+  for (let i = row, j = col; i < board.length && j < board.length; i++, j++) {
+    if (board[i][j]) {
+      return false;
+    }
+  }
+  // Upper right  diagonal 
+  for (i=row, j=col; j < board.length && i >= 0; i--, j++) {
+    if (board[i][j]) {
+      return false;
+    }
+  }
+  return true;
 }
 
 function queens(board, col) {
@@ -78,10 +58,14 @@ function queens(board, col) {
 }
 
 
-let board = [[0 ,0, 0, 0],
-            [0 ,0, 0, 0],
-            [0 ,0, 0, 0],
-            [0 ,0, 0, 0]];
+let board = [[0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0],]
 
 if (queens(board, 0)) {
   console.log(board)
