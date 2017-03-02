dad58fe30a9d21b9971b56e6e4c378bbf42b48de
Juan Carlos <juancafe2@gmail.com>
2017-03-02 15:16:48 -0400

    Radix sort

--- concepts_and_algorithms/sorting_and_searching/radix_sort.js
+++ concepts_and_algorithms/sorting_and_searching/radix_sort.js
@@ -1,7 +1,33 @@
-var nums = [35, 25, 53, 3, 102, 203, 230, 1005];
-var k = Math.max.apply(null, nums.map(i => {
-    console.log(Math.ceil(Math.log(i)/Math.log(2)));
-    return Math.ceil(Math.log(i)/Math.log(2));
-}));
+// Runtime O(kn) O(K + n)
+function countSort(arr, exp) {
+  let bucket = {};
+  for (let i = 0; i < 10; i++ ) {
+    bucket[i] = [];
+  }
 
-console.log(k)
+  for (let i = 0; i < arr.length; i++) {
+    bucket[Math.round(arr[i]/exp % 10)].push(arr[i]);
+  }
+
+  let output = [];
+
+  for (let i = 0; i < 10; i++ ) {
+    output.push(...bucket[i]);
+  }
+  for (let i = 0; i < arr.length; i++) {
+    arr[i] = output[i];
+  }
+}
+function radixSort(arr) {
+  let max = Math.max(...arr);
+
+  for (let exp = 1; max/exp > 0; exp *= 10) {
+    countSort(arr, exp);
+  }
+
+  console.log(arr)
+}
+
+
+radixSort([170, 45, 75, 90, 802, 24, 2, 66])
+radixSort([1, 45, 759, 2, 24, 2, 6])
