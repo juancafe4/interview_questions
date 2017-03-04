73f7b824559626ee593de0eca64af54f55958f6d
Juan Carlos <juancafe2@gmail.com>
2017-03-03 22:47:43 -0400

    Binary search with recursion

--- concepts_and_algorithms/sorting_and_searching/binary_search.js
+++ concepts_and_algorithms/sorting_and_searching/binary_search.js
@@ -19,9 +19,25 @@
 //   return 'not found';
 // }
 
-function binary_search
+function binary_search(arr, x, low, high) {
+  if (low <= high) {
+    let mid = Math.floor((low + high) / 2);
+
+    if (arr[mid] < x) {
+      low = mid + 1;
+      return binary_search(arr, x, low, high);
+    }
+    else if (arr[mid] > x) {
+      high = mid - 1;
+      return binary_search(arr, x, low, high);
+    } else {
+      return x;
+    }
+  }
+  return 'not found';
+}
 
 let arr1 = [-4, -3, 3, 4, 5, 55];
 
-console.log(binary_search(arr1, 4));
-console.log(binary_search(arr1, 2));
+console.log(binary_search(arr1, 4, 0, arr1.length - 1));
+console.log(binary_search(arr1, 2, 0, arr1.length - 1));
